3e0724e27349b34614c94e14403bc44abd94a862
Kho Kai Hong <khokaihong@gmail.com>
2024-11-25 14:14:09 +0800

    finsihed user notification

--- Backend/src/model/user_notification.rs
+++ Backend/src/model/user_notification.rs
@@ -83,7 +83,10 @@ impl UserNotificationModelController {
         Ok(notification)
     }
 
-    pub async fn list(context: &Context, model_manager: &ModelManager) -> Result<Vec<UserNotification>> {
+    pub async fn list(
+        context: &Context,
+        model_manager: &ModelManager,
+    ) -> Result<Vec<UserNotification>> {
         let db = model_manager.db();
 
         let notifications = sqlx::query_as("SELECT * FROM user_notifications ORDER BY id")
@@ -93,13 +96,17 @@ impl UserNotificationModelController {
         Ok(notifications)
     }
 
-    pub async fn list_by_user_id(context: &Context, model_manager: &ModelManager) -> Result<Vec<UserNotification>> {
+    pub async fn list_by_user_id(
+        context: &Context,
+        model_manager: &ModelManager,
+    ) -> Result<Vec<UserNotification>> {
         let db = model_manager.db();
 
-        let notifications = sqlx::query_as("SELECT * FROM user_notifications WHERE user_id = $1 ORDER BY id")
-            .bind(context.user_id())
-            .fetch_all(db)
-            .await?;
+        let notifications =
+            sqlx::query_as("SELECT * FROM user_notifications WHERE user_id = $1 ORDER BY id")
+                .bind(context.user_id())
+                .fetch_all(db)
+                .await?;
 
         Ok(notifications)
     }
@@ -112,6 +119,7 @@ impl UserNotificationModelController {
         let db = model_manager.db();
 
         let count = sqlx::query("UPDATE user_notifications SET is_read = true WHERE id = $1")
+            .bind(id)
             .execute(db)
             .await?
             .rows_affected();
@@ -133,9 +141,10 @@ impl UserNotificationModelController {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{_dev_utils, model::user};
+    use crate::{_dev_utils, auth::Role};
     use anyhow::Result;
     use serial_test::serial;
+    use uuid::Uuid;
 
     #[tokio::test]
     #[serial]
@@ -143,22 +152,24 @@ mod tests {
         // -- Setup & Fixtures
         let model_manager = _dev_utils::init_test().await;
         let context = Context::root_ctx();
-        let notification_created = UserNotificationForCreate { 
+        let notification_created = UserNotificationForCreate {
             description: "test_description".to_string(),
             redirect: None,
-            user_id: 1,
+            user_id: 1000,
         };
 
         // -- Exec
         let id =
-            UserNotificationModelController::create(&context, &model_manager, notification_created).await?;
+            UserNotificationModelController::create(&context, &model_manager, notification_created)
+                .await?;
 
         // -- Check
-        let notification = UserNotificationModelController::get(&context, &model_manager, id).await?;
+        let notification =
+            UserNotificationModelController::get(&context, &model_manager, id).await?;
         assert_eq!(notification.id, id);
         assert_eq!(notification.redirect, None);
         assert_eq!(notification.description, "test_description");
-        assert_eq!(notification.user_id, 1);
+        assert_eq!(notification.user_id, 1000);
         assert_eq!(notification.is_read, false);
 
         println!("\n\nnotification: {:?}", notification);
@@ -205,48 +216,55 @@ mod tests {
         let model_manager = _dev_utils::init_test().await;
         let context = Context::root_ctx();
 
-        let organiser_created1 = OrganiserForCreate {
-            email: "test_email1@example.com".to_string(),
-            password: "welcome1".to_string(),
-            name: "Test Organiser 01".to_string(),
-            phone_number: "1234567890".to_string(),
+        let notification_created1 = UserNotificationForCreate {
+            description: "test_description1".to_string(),
+            redirect: None,
+            user_id: 1000,
         };
-        let organiser_created2 = OrganiserForCreate {
-            email: "test_email2@example.com".to_string(),
-            password: "welcome2".to_string(),
-            name: "Test Organiser 02".to_string(),
-            phone_number: "987654321".to_string(),
+        let notification_created2 = UserNotificationForCreate {
+            description: "test_description2".to_string(),
+            redirect: Some(String::from("event")),
+            user_id: 1001,
         };
 
-        let id1 =
-            OrganiserModelController::create(&context, &model_manager, organiser_created1).await?;
-        let id2 =
-            OrganiserModelController::create(&context, &model_manager, organiser_created2).await?;
-        let organisers = OrganiserModelController::list(&context, &model_manager).await?;
+        let id1 = UserNotificationModelController::create(
+            &context,
+            &model_manager,
+            notification_created1,
+        )
+        .await?;
+        let id2 = UserNotificationModelController::create(
+            &context,
+            &model_manager,
+            notification_created2,
+        )
+        .await?;
+        let notifications: Vec<UserNotification> =
+            UserNotificationModelController::list(&context, &model_manager).await?;
 
         // Check
-        assert_eq!(organisers.len(), 5, "Number of organisers");
-        assert_eq!(organisers[3].id, id1);
-        assert_eq!(organisers[4].id, id2);
-        assert_eq!(organisers[3].email, "test_email1@example.com");
-        assert_eq!(organisers[4].email, "test_email2@example.com");
-        assert_eq!(organisers[3].password, "welcome1");
-        assert_eq!(organisers[4].password, "welcome2");
-        assert_eq!(organisers[3].name, "Test Organiser 01");
-        assert_eq!(organisers[4].name, "Test Organiser 02");
-        assert_eq!(organisers[3].phone_number, "1234567890");
-        assert_eq!(organisers[4].phone_number, "987654321");
+        assert_eq!(notifications.len(), 2);
+        assert_eq!(notifications[0].id, id1);
+        assert_eq!(notifications[1].id, id2);
+        assert_eq!(notifications[0].description, "test_description1");
+        assert_eq!(notifications[1].description, "test_description2");
+        assert_eq!(notifications[0].redirect, None);
+        assert_eq!(notifications[1].redirect, Some(String::from("event")));
+        assert_eq!(notifications[0].user_id, 1000);
+        assert_eq!(notifications[1].user_id, 1001);
+        assert_eq!(notifications[0].is_read, false);
+        assert_eq!(notifications[1].is_read, false);
 
-        for organiser in organisers.iter() {
-            println!("organiser: {:?}", organiser);
+        for notification in notifications.iter() {
+            println!("notification: {:?}", notification);
         }
 
         // Clean
-        sqlx::query("DELETE FROM event_organisers WHERE id = $1")
+        sqlx::query("DELETE FROM user_notifications WHERE id = $1")
             .bind(id1)
             .execute(model_manager.db())
             .await?;
-        sqlx::query("DELETE FROM event_organisers WHERE id = $1")
+        sqlx::query("DELETE FROM user_notifications WHERE id = $1")
             .bind(id2)
             .execute(model_manager.db())
             .await?;
@@ -256,41 +274,76 @@ mod tests {
 
     #[tokio::test]
     #[serial]
-    async fn test_update_ok() -> Result<()> {
+    async fn test_list_by_user_id() -> Result<()> {
         // -- Setup & Fixtures
         let model_manager = _dev_utils::init_test().await;
-        let context = Context::root_ctx();
-        let organiser_created = OrganiserForCreate {
-            email: "test_list_ok@example.com".to_string(),
-            password: "welcome".to_string(),
-            name: "Test Organiser 01".to_string(),
-            phone_number: "1234567890".to_string(),
+        let context = Context::new(1000, Role::User, Uuid::new_v4());
+
+        let notification_created1 = UserNotificationForCreate {
+            description: "test_description1".to_string(),
+            redirect: None,
+            user_id: 1000,
+        };
+        let notification_created2 = UserNotificationForCreate {
+            description: "test_description2".to_string(),
+            redirect: Some(String::from("event")),
+            user_id: 1001,
+        };
+        let notification_created3 = UserNotificationForCreate {
+            description: "test_description3".to_string(),
+            redirect: Some(String::from("event")),
+            user_id: 1000,
         };
 
-        let id =
-            OrganiserModelController::create(&context, &model_manager, organiser_created).await?;
+        let id1 = UserNotificationModelController::create(
+            &context,
+            &model_manager,
+            notification_created1,
+        )
+        .await?;
+        let id2 = UserNotificationModelController::create(
+            &context,
+            &model_manager,
+            notification_created2,
+        )
+        .await?;
+        let id3 = UserNotificationModelController::create(
+            &context,
+            &model_manager,
+            notification_created3,
+        )
+        .await?;
+        let notifications: Vec<UserNotification> =
+            UserNotificationModelController::list_by_user_id(&context, &model_manager).await?;
 
-        let organiser_updated = OrganiserForUpdate {
-            email: Some("new_email@gmail.com".to_string()),
-            password: None,
-            name: Some("New name".to_string()),
-            phone_number: None,
-        };
+        // Check
+        assert_eq!(notifications.len(), 2);
+        assert_eq!(notifications[0].id, id1);
+        assert_eq!(notifications[1].id, id3);
+        assert_eq!(notifications[0].description, "test_description1");
+        assert_eq!(notifications[1].description, "test_description3");
+        assert_eq!(notifications[0].redirect, None);
+        assert_eq!(notifications[1].redirect, Some(String::from("event")));
+        assert_eq!(notifications[0].user_id, 1000);
+        assert_eq!(notifications[1].user_id, 1000);
+        assert_eq!(notifications[0].is_read, false);
+        assert_eq!(notifications[1].is_read, false);
 
-        OrganiserModelController::update(&context, &model_manager, id, organiser_updated).await?;
-
-        // -- Check
-        let organiser = OrganiserModelController::get(&context, &model_manager, id).await?;
-        assert_eq!(organiser.email, "new_email@gmail.com");
-        assert_eq!(organiser.password, "welcome");
-        assert_eq!(organiser.name, "New name");
-        assert_eq!(organiser.phone_number, "1234567890");
-
-        println!("\n\norganiser: {:?}", organiser);
+        for notification in notifications.iter() {
+            println!("notification: {:?}", notification);
+        }
 
         // Clean
-        sqlx::query("DELETE FROM event_organisers WHERE id = $1")
-            .bind(id)
+        sqlx::query("DELETE FROM user_notifications WHERE id = $1")
+            .bind(id1)
+            .execute(model_manager.db())
+            .await?;
+        sqlx::query("DELETE FROM user_notifications WHERE id = $1")
+            .bind(id2)
+            .execute(model_manager.db())
+            .await?;
+        sqlx::query("DELETE FROM user_notifications WHERE id = $1")
+            .bind(id3)
             .execute(model_manager.db())
             .await?;
 
@@ -299,65 +352,39 @@ mod tests {
 
     #[tokio::test]
     #[serial]
-    async fn get_by_email_ok() -> Result<()> {
+    async fn test_read_notification() -> Result<()> {
         // -- Setup & Fixtures
         let model_manager = _dev_utils::init_test().await;
         let context = Context::root_ctx();
-        let organiser_created = OrganiserForCreate {
-            email: "test_create_ok@example.com".to_string(),
-            password: "welcome".to_string(),
-            name: "Test Organiser".to_string(),
-            phone_number: "1234567890".to_string(),
+
+        let notification_created = UserNotificationForCreate {
+            description: "test_description".to_string(),
+            redirect: Some(String::from("event")),
+            user_id: 1000,
         };
 
-        let id =
-            OrganiserModelController::create(&context, &model_manager, organiser_created).await?;
-
         // -- Exec
-        let organiser =
-            OrganiserModelController::get_by_email(&model_manager, "test_create_ok@example.com")
+        let id =
+            UserNotificationModelController::create(&context, &model_manager, notification_created)
                 .await?;
 
-        // -- Check
-        assert_eq!(organiser.email, "test_create_ok@example.com");
-        assert_eq!(organiser.password, "welcome");
-        assert_eq!(organiser.name, "Test Organiser");
-        assert_eq!(organiser.phone_number, "1234567890");
+        UserNotificationModelController::read_notification(&context, &model_manager, id).await?;
 
-        println!("\n\norganiser: {:?}", organiser);
+        // -- Check
+        let notification = UserNotificationModelController::get(&context, &model_manager, id).await?;
+        assert_eq!(notification.id, id);
+        assert_eq!(notification.description, "test_description");
+        assert_eq!(notification.redirect, Some(String::from("event")));
+        assert_eq!(notification.user_id, 1000);
+        assert_eq!(notification.is_read, true);
+
+        println!("\n\nnotification: {:?}", notification);
 
         // Clean
-        sqlx::query("DELETE FROM event_organisers WHERE id = $1")
+        sqlx::query("DELETE FROM user_notifications WHERE id = $1")
             .bind(id)
             .execute(model_manager.db())
             .await?;
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    #[serial]
-    async fn get_by_email_err_not_found() -> Result<()> {
-        // -- Setup & Fixtures
-        let model_manager = _dev_utils::init_test().await;
-
-        // -- Exec
-        let res =
-            OrganiserModelController::get_by_email(&model_manager, "test_list_ok@example.com")
-                .await;
-
-        // -- Check
-        assert!(
-            matches!(
-                res,
-                Err(Error::EntityNotFound {
-                    entity: "organiser",
-                    field: StringError(ref e)
-                }) if e == "test_list_ok@example.com"
-            ),
-            "Expected EntityNotFound error, got: {:?}",
-            res
-        );
 
         Ok(())
     }
